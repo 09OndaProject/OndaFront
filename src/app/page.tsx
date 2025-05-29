f9f464f37b65ccd1142ea3d44853edb80b8c3a08
yurim <kyl6682@gmail.com>
2025-05-29 16:19:42 +0900

    💄 Style : #1 테일윈드 컬러 및 폰트 시스템 등록

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="p-4">
-      <h2 className="font-bold text-gray-200">안녕</h2>
+      <h2 className="text-2xl text-primary hover:text-primary-deep">안녕</h2>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,46 @@ module.exports = {
   ],
   theme: {
     extend: {
-    },
-    fontFamily: {
-      sans: ['"Noto Sans Korean"', 'sans-serif'],
+      fontSize: {
+        xs: ["1.25rem", "1.5rem"], // 20px / 24px
+        sm: ["1.5rem", "1.75rem"], // 24px / 28px
+        md: ["1.75rem", "2rem"], // 28px / 32px
+        lg: ["2rem", "2.25rem"], // 32px / 36px
+        xl: ["2.25rem", "2.5rem"], // 36px / 40px
+        "2xl": ["2.5rem", "2.75rem"], // 40px / 44px
+        "3xl": ["2.75rem", "3rem"], // 44px / 48px
+      },
+      colors: {
+        main: "#333333",
+
+        primary: {
+          DEFAULT: "#FFB347",
+          light: "#FFDCAA",
+          deep: "#F69306",
+        },
+
+        // Sub Colors
+        "sub-green": "#7FB77E",
+        "sub-blue": "#B3CDE0",
+
+        // Accent Colors
+        "accent-red": "#FF4B4A",
+        "accent-blue": "#2D60FF",
+        "accent-main": "#F26B38",
+
+        // Gray Scale
+        "gray-800": "#222222",
+        "gray-700": "#555555",
+        "gray-600": "#888888",
+        "gray-500": "#C3C3C3",
+        "gray-400": "#DEDEDE",
+        "gray-300": "#E8E9EF",
+        "gray-200": "#F5F7FA",
+        "gray-100": "#FAFBFC",
+      },
+      fontFamily: {
+        sans: ["Noto Sans Korean", "sans-serif"],
+      },
     },
   },
   plugins: [],
