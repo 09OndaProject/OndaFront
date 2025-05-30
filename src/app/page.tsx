27c95ad8aa4486308229edcfa098b285d07e817f
yurim <kyl6682@gmail.com>
2025-05-30 15:56:48 +0900

    ✨ Feature : #24 홈페이지 Main Banner 섹션 화면 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/homeComponents/HeroSection";
+import MainBannerSection from "@/components/homeComponents/MainBannerSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <MainBannerSection />
     </main>
   );
 }
--- src/components/homeComponents/HeroSection.tsx
+++ src/components/homeComponents/HeroSection.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import heroImage from "@/assets/images/hero.png";
@@ -8,45 +8,37 @@ import { useRouter } from "next/navigation";
 
 export default function HeroSection() {
   const router = useRouter();
+
   return (
-    <>
-      <div className="flex items-center flex-col h-[670px]">
-        <div className="flex items-center justify-center text-center gap-40 w-full h-[500px] bg-gradient-to-t from-[#FDE8E0] to-transparent">
-          {/* 왼쪽 텍스트 + 버튼 영역 */}
-          <div className="flex flex-col gap-4 text-center items-center">
-            <div className="mb-5 sm:mb-20 z-10">
-              <div>
-                <h1 className="text-2xl font-bold py-10">
-                  공간, 경험, 지식 <br />
-                  그리고 사람을 잇다
-                </h1>
-              </div>
-              <div className="flex gap-4">
-                <Button onClick={() => router.push("/#")}>시작하기</Button>
-                <Button
-                  color="gray"
-                  variant="outline"
-                  onClick={() => router.push("/#")}
-                >
-                  리더 신청
-                </Button>
-              </div>
-            </div>
-            <div className="absolute bottom-[100px] w-[800px] h-[400px] bg-[#FFDEAD] rounded-t-full z-0 hidden md:block" />
+    <div className="relative w-full h-[670px] bg-gradient-to-t from-[#FDE8E0] to-transparent">
+      {/* 콘텐츠 */}
+      <div className="relative z-20 h-full flex items-center justify-center gap-40 px-4">
+        <div className="relative flex flex-col items-center md:text-left md:items-center space-y-6 z-10">
+          <h1 className="text-3xl sm:text-3xl font-bold  text-center mb-10 leading-relaxed">
+            공간, 경험, 지식 <br />
+            그리고 사람을 잇다
+          </h1>
+          <div className="flex gap-4">
+            <Button onClick={() => router.push("/#")}>시작하기</Button>
+            <Button color="gray" variant="outline" onClick={() => router.push("/#")}>
+              리더 신청
+            </Button>
           </div>
-          {/* 오른쪽 이미지 영역 */}
-          <div className="hidden xl:block bg-transparent z-30">
-            <Image
-              className="max-w-[650px] relative bottom-[-30px]"
-              src={heroImage.src}
-              alt="hero"
-              width={450}
-              height={650}
-            />
-          </div>
+          <div className="absolute left-1/2 -translate-x-1/2 w-[600px] h-[300px] bg-[#FFDEAD] rounded-t-full z-[-1] hidden md:block" />
         </div>
-        <div className="h-[100px] w-full bg-white z-20"></div>
+
+        {/* 오른쪽 이미지 */}
+        <div className="hidden md:block z-30">
+          <Image
+            className="relative bottom-[-30px] max-w-[650px]"
+            src={heroImage}
+            alt="hero"
+            width={450}
+            height={650}
+          />
+        </div>
       </div>
-    </>
+      <div className="absolute bottom-0 w-full h-[120px] bg-white z-10" />
+    </div>
   );
 }
