9025689dcb2b011db8857186d9a37813055c19e4
sewook <makato08@naver.com>
2025-06-02 14:49:01 +0900

    ✨feat:#34 홈에서 모임찾기 경로 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
       {/* 중앙 */}
       <nav className="flex gap-10 text-black font-medium text-base">
         <Link href={"/"}>홈으로</Link>
-        <Link href={"#"}>모임찾기</Link>
+        <Link href={"/meet/search"}>모임찾기</Link>
         <Link href={"#"}>리더신청</Link>
         <Link href={"#"}>소통하기</Link>
         <Link href={"#"}>온:다 소개</Link>
