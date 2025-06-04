f9feba1415ddf88c8bf8326d4ecbd22fee1c602b
yurim <kyl6682@gmail.com>
2025-06-04 19:03:09 +0900

    ✨ Feature: #51 게시글 검색 섹션 추가

--- src/app/community/_components/PostSearch.tsx
+++ src/app/community/_components/PostSearch.tsx
@@ -1,8 +1,13 @@
-import SelectBox from '@/components/common/SelectBox'
-import TextInput from '@/components/common/TextInput'
-import { categoryOptions, digitalLevelOptions, interestOptions } from '@/constants/category';
-import { Search } from 'lucide-react'
-import React, { useState } from 'react'
+import Button from "@/components/common/Button";
+import SelectBox from "@/components/common/SelectBox";
+import TextInput from "@/components/common/TextInput";
+import {
+  categoryOptions,
+  digitalLevelOptions,
+  interestOptions,
+} from "@/constants/category";
+import { Search } from "lucide-react";
+import React, { useState } from "react";
 
 export default function PostSearch() {
   const [selectedCategory, setSelectedCategory] = useState(0);
@@ -10,14 +15,41 @@ export default function PostSearch() {
   const [selectedArea, setSelectedArea] = useState(0);
   const [selectedDigitalLevel, setSelectedDigitalLevel] = useState(0);
 
-
   return (
-    <div className='w-full flex flex-wrap gap-4'>
-      <TextInput placeholder="제목으로 게시글을 검새해보세요." icon={<Search size={24} />} className="w-full rounded-full h-12 text-center" ></TextInput>
-      <SelectBox value={selectedCategory} options={categoryOptions} onChange={(e) => setSelectedCategory(Number(e.target.value))}/>
-      <SelectBox value={selectedInterest} options={interestOptions} onChange={(e) => setSelectedInterest(Number(e.target.value))}/>
-      <SelectBox value={selectedArea} options={interestOptions} onChange={(e) => setSelectedArea(Number(e.target.value))}/>
-      <SelectBox value={selectedDigitalLevel} options={digitalLevelOptions} onChange={(e) => setSelectedDigitalLevel(Number(e.target.value))}/>
+    <div className="w-full flex flex-col gap-8 border-b-2 py-10">
+      <TextInput
+        placeholder="제목으로 게시글을 검새해보세요."
+        icon={<Search size={24} />}
+        className="w-full"
+      ></TextInput>
+      <div className="flex flex-wrap gap-4 justify-center">
+        <SelectBox
+          value={selectedCategory}
+          options={categoryOptions}
+          placeholder="카테고리"
+          onChange={(e) => setSelectedCategory(Number(e.target.value))}
+        />
+        <SelectBox
+          value={selectedInterest}
+          options={interestOptions}
+          placeholder="관심사"
+          onChange={(e) => setSelectedInterest(Number(e.target.value))}
+          className="flex-grow"
+        />
+        <SelectBox
+          value={selectedArea}
+          options={interestOptions}
+          placeholder="지역"
+          onChange={(e) => setSelectedArea(Number(e.target.value))}
+        />
+        <SelectBox
+          value={selectedDigitalLevel}
+          options={digitalLevelOptions}
+          placeholder="디지털 난이도"
+          onChange={(e) => setSelectedDigitalLevel(Number(e.target.value))}
+        />
+        <Button>검색</Button>
+      </div>
     </div>
-  )
+  );
 }
