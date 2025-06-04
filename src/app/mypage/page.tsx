dcbcd1bdc206966e05926cf651b654a7108dfb39
sewook <makato08@naver.com>
2025-06-04 16:39:56 +0900

    💩feat:#49 완료된 모임 상세 정보 모달 화면 UI 구성(미완성)

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -5,8 +5,22 @@ import AppliedScheduleList from "./_components/AppliedScheduleList";
 import PastScheduleList from "./_components/PastScheduleList";
 import { useAuthStore } from "@/stores/useAuth";
 import { useRouter } from "next/navigation";
+import { useModalStore } from "@/stores/useModalStore";
+import FinishedMeetDetailModal from "../meet/detail/_components/FinishedMeetDetailModal";
 
 export default function Mypage() {
+  const { modals, closeModal } = useModalStore();
+
+  // dummyData는 현재는 하드코딩, 나중엔 props나 API로 대체 가능
+  const dummyData = {
+    title: "걷기 & 대화 모임",
+    date: "2025-06-04",
+    location: "서울특별시 어쩌구 저쩌구",
+    descrlption: "가볍게 함께 걸으며\n건강도 챙기고 이웃과 마음을 나눠요",
+    image: null,
+    leaderName: "리더 이름",
+    leaderImage: null,
+  };
   const { currentUser } = useAuthStore();
   console.log(currentUser?.name);
   const router = useRouter();
@@ -35,6 +49,10 @@ export default function Mypage() {
         </button>
       </div>
       <PastScheduleList />
+      <FinishedMeetDetailModal
+        data={dummyData}
+        onClose={() => closeModal("finishedMeetDetail")}
+      />
     </main>
   );
 }
