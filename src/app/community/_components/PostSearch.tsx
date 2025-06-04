03b3cc657d8e66d287137cf425a68a843fd99fe5
yurim <kyl6682@gmail.com>
2025-06-04 11:59:41 +0900

    ✨ Feature: #51 커뮤니티 목록 페이지 라우팅

---
+++ src/app/community/_components/PostSearch.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PostSearch() {
+  return (
+    <div>PostSearch</div>
+  )
+}
---
+++ src/app/community/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PostSearch from './_components/PostSearch'
+import PostList from './_components/PostList'
+
+export default function page() {
+  return (
+    <div>
+        <PostSearch />
+        <PostList />
+    </div>
+  )
+}
