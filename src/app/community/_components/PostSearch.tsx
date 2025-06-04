5770baf938576025efbff212a1e3edbebc7ec3de
yurim <kyl6682@gmail.com>
2025-06-04 18:45:49 +0900

    ♻️ Refactor: #51 공통 Input 컴포넌트 리팩토링
    TextInput 컴포넌트 Props 추가, SelectBox value 타입 추가

--- src/app/community/_components/PostSearch.tsx
+++ src/app/community/_components/PostSearch.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import SelectBox from '@/components/common/SelectBox'
+import TextInput from '@/components/common/TextInput'
+import { categoryOptions, digitalLevelOptions, interestOptions } from '@/constants/category';
+import { Search } from 'lucide-react'
+import React, { useState } from 'react'
 
 export default function PostSearch() {
+  const [selectedCategory, setSelectedCategory] = useState(0);
+  const [selectedInterest, setSelectedInterest] = useState(0);
+  const [selectedArea, setSelectedArea] = useState(0);
+  const [selectedDigitalLevel, setSelectedDigitalLevel] = useState(0);
+
+
   return (
-    <div>PostSearch</div>
+    <div className='w-full flex flex-wrap gap-4'>
+      <TextInput placeholder="제목으로 게시글을 검새해보세요." icon={<Search size={24} />} className="w-full rounded-full h-12 text-center" ></TextInput>
+      <SelectBox value={selectedCategory} options={categoryOptions} onChange={(e) => setSelectedCategory(Number(e.target.value))}/>
+      <SelectBox value={selectedInterest} options={interestOptions} onChange={(e) => setSelectedInterest(Number(e.target.value))}/>
+      <SelectBox value={selectedArea} options={interestOptions} onChange={(e) => setSelectedArea(Number(e.target.value))}/>
+      <SelectBox value={selectedDigitalLevel} options={digitalLevelOptions} onChange={(e) => setSelectedDigitalLevel(Number(e.target.value))}/>
+    </div>
   )
 }
