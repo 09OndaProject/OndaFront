ee5bf4e9b0c0ca58053c7cab47d3a028b9e6bf9b
yurim <kyl6682@gmail.com>
2025-06-05 17:03:12 +0900

    📝 Docs: #63 Post 타입에 작성자 여부 타입 추가, 댓글 타입 및 더미 데이터 생성

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,13 +1,26 @@
 export type Post = {
-    post_id: number;
-    title: string;
-    content: string;
-    category_id: number | null;
-    digitalLevel_id: number | null;
-    nickname : string;
-    area_id: number | null;
-    interest_id : number | null;
-    image_url?: string;
-    created_at: Date;
-    updated_at?: Date;
-}
+  post_id: number;
+  title: string;
+  content: string;
+  category_id: number | null;
+  digitalLevel_id: number | null;
+  nickname: string;
+  area_id: number | null;
+  interest_id: number | null;
+  image_url?: string;
+  created_at: Date;
+  updated_at?: Date;
+  is_author: boolean;
+  //   likes: number;
+  //   is_liked: boolean;
+};
+
+export type Comment = {
+  post_id: number;
+  comment_id: number;
+  nickname: string;
+  content: string;
+  created_at: Date;
+  updated_at?: Date;
+  is_author: boolean;
+};
