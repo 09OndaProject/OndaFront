0a65c6adad1720d5514aad090cdf7a5dbca20514
yurim <kyl6682@gmail.com>
2025-06-09 16:54:20 +0900

    📝 Chore: #63 작업 내용 저장

--- src/app/community/[postId]/_components/PostContent.tsx
+++ src/app/community/[postId]/_components/PostContent.tsx
@@ -3,12 +3,11 @@ import Image from "next/image";
 import React from "react";
 
 export default function PostContent({ image, content }: PostContentProps) {
+
   return (
     <div className="space-y-2 w-full py-10 border-b-2 border-gray-400 min-h-[400px]">
       {image && (
         <div className="relative w-full h-[600px] mb-4">
-          {" "}
-          {/* 높이 지정 필수 */}
           <Image
             src={image}
             alt="게시글 이미지"
