9b5d9b67fb576c8ea24a69569a9191828cebb09b
yurim <kyl6682@gmail.com>
2025-06-16 16:27:42 +0900

    ♻️ Refactor: #107 게시글 카테고리 api 연동

--- src/app/community/_components/DropdownInput.tsx
+++ src/app/community/_components/DropdownInput.tsx
@@ -10,9 +10,10 @@ interface Option {
 
 interface DropdownInput {
   value?: number;
-  onChange: (value: number | undefined) => void;
+  onChange: (value: number) => void;
   options: Option[];
   placeholder?: string;
+  className?: string;
 }
 
 export default function DropdownInput({
@@ -20,6 +21,7 @@ export default function DropdownInput({
   onChange,
   options,
   placeholder = "선택하세요.",
+  className,
 }: DropdownInput) {
   const [open, setOpen] = useState(false);
   const selectRef = useRef<HTMLDivElement>(null);
@@ -37,34 +39,38 @@ export default function DropdownInput({
   }, []);
 
   return (
-    <div className="relative w-full" ref={selectRef}>
-      <button
-        type="button"
-        className="flex justify-between w-full bg-white border border-gray-300 rounded-md "
-        onClick={() => setOpen((prev) => !prev)}
-      >
-        <span className={selected ? "text-black" : "text-gray-600"}>
-          {selected ? selected.label : placeholder}
-        </span>
-        {open ? <ChevronUp /> : <ChevronDown />}
-      </button>
+    <div className={`${className}`}>
+      <div className="relative w-full" ref={selectRef}>
+        <button
+          type="button"
+          className="flex items-center justify-between w-full bg-white border border-gray-300 rounded-md px-4 py-2 text-sm"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          <span className={selected ? "text-black" : "text-gray-600"}>
+            {selected ? selected.label : placeholder}
+          </span>
+          {open ? <ChevronUp /> : <ChevronDown />}
+        </button>
 
-      {open && (
-        <ul className="absolute z-10 w-full bg-white border mt-1 rounded-md shadow-md max-h-60 overflow-auto">
-          {options.map((opt) => (
-            <li
-              className={`px-2 py-1 m-1 hover:bg-gray-100 cursor-pointer rounded-md ${value === opt.value ? "bg-gray-100 font-medium" : ""}`}
-              key={opt.value}
-              onClick={() => {
-                onChange(opt.value);
-                setOpen(false);
-              }}
-            >
-              {opt.label}
-            </li>
-          ))}
-        </ul>
-      )}
+        {open && (
+          <ul className="text-sm absolute z-10 w-full bg-white border mt-1 rounded-md shadow-md max-h-60 overflow-auto">
+            {options.map((opt) => (
+              <li
+                className={`px-2 py-4 m-1 hover:bg-gray-200 cursor-pointer rounded-md ${value === opt.value ? "bg-gray-100 font-medium" : ""}`}
+                key={opt.value}
+                onClick={() => {
+                  if (typeof opt.value === "number") {
+                    onChange(opt.value);
+                    setOpen(false);
+                  }
+                }}
+              >
+                {opt.label}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
