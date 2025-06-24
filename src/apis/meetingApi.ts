e71eb6515138adc665e9b18cd6602c483836b1d8
hyewonny2327 <wonny2327@gmail.com>
2025-06-24 17:19:07 +0900

    ♻️ refactor : 함수 중복 로직 제거

--- src/apis/meetingApi.ts
+++ src/apis/meetingApi.ts
@@ -1,16 +1,5 @@
 import api from '@/apis/app';
 
-export async function getCategoryOptions() {
-  const response = await api.get('/options/categories');
-  console.log('response', response.data);
-  return response.data;
-}
-
-export async function getDigitalLevelOptions() {
-  const response = await api.get('/options/digital-levels');
-  return response.data;
-}
-
 export async function getMeetDetail(meet_id: number) {
   const response = await api.get(`/meets/${meet_id}`);
   return response.data;
