7eb4a3b69d02b6925121cf65e1822473c5c685f9
yurim <kyl6682@gmail.com>
2025-06-25 18:46:22 +0900

    🐛 Fix: #154 게시글 검색 및 필터링 로직 수정

--- src/hooks/useFilteredPosts.ts
+++ src/hooks/useFilteredPosts.ts
@@ -7,10 +7,10 @@ export function useFilteredPosts(searchParams: SearchParams, page: number) {
   const { data, isLoading } = useFetchPostList(page);
 
   const hasFilter =
-    searchParams.category.id !== 0 ||
-    searchParams.interest.id !== 0 ||
-    searchParams.area?.childId.id !== 0 ||
-    searchParams.keyword.trim() !== "";
+    !!searchParams.category?.id ||
+    !!searchParams.interest?.id ||
+    !!searchParams.area?.childId.id ||
+    !!searchParams.keyword?.trim();
 
   const filtered = useMemo<Post[]>(() => {
     const posts = data?.results ?? [];
@@ -18,22 +18,22 @@ export function useFilteredPosts(searchParams: SearchParams, page: number) {
 
     return posts.filter((post) => {
       const filteredCategory =
-        !searchParams.category || post.category.id === searchParams.category.id;
+        searchParams.category?.id === 0 ||
+        post.category.id === searchParams.category.id;
 
       const filteredInterest =
-        !searchParams.interest || post.interest?.id === searchParams.interest.id;
+        searchParams.interest?.id === 0 ||
+        post.interest?.id === searchParams.interest.id;
 
       const filteredArea =
-        !searchParams.area?.childId || post.area?.id === searchParams.area.childId.id;
+        searchParams.area?.childId?.id === 0 ||
+        post.area?.id === searchParams.area.childId.id;
 
       const filteredKeyword =
         !searchParams.keyword || post.title.includes(searchParams.keyword);
 
       return (
-        filteredCategory &&
-        filteredInterest &&
-        filteredArea &&
-        filteredKeyword
+        filteredCategory && filteredInterest && filteredArea && filteredKeyword
       );
     });
   }, [data, searchParams, hasFilter]);
